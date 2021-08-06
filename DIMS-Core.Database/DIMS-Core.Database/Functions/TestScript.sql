5bd8c0e8995cda36fc1838d96b73268ea8c48cbc
Konstantin Borovik <kes8594@yandex.ru>
2021-08-06 11:57:30 +0300

    - some fixes for DB proj

--- DIMS-Core.Database/DIMS-Core.Database/Functions/TestScript.sql
+++
@@ -1,9 +0,0 @@
-﻿-- tests for functions --
--- GetFullAge
-DECLARE @BirthOfDate DATETIME;
-
-SET @BirthOfDate = '1993-10-28';
-
-SELECT dbo.GetFullAge(@BirthOfDate)
-
--- add here next one function with sample how to chek it
